40a539c7fb9e0fed885b002c94691cc193e11455
Welly <welly@MacBook-Pro.local>
2019-06-20 21:46:53 +1000

    add decorate test

--- lib/model/organization_test.go
+++ lib/model/organization_test.go
@@ -38,3 +38,28 @@ func TestOrganizationsSearch(t *testing.T) {
 		})
 	}
 }
+
+func TestOrganizationDecorate(t *testing.T) {
+	testCases := []struct {
+		desc                     string
+		organizationSearchResult OrganizationSearchResult
+		expected                 []int
+	}{
+		{
+			desc:                     "Organization result is decorated succesfully, its number of users and tickets is 1 not 0",
+			organizationSearchResult: MockOrganizationSearchResult,
+			expected:                 []int{1, 1, 1, 1},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.desc, func(t *testing.T) {
+			testCase.organizationSearchResult.Decorate(MockDataSet)
+			assert.Equal(t, testCase.expected[0], len(testCase.organizationSearchResult.Items[0].Tickets))
+			assert.Equal(t, testCase.expected[1], len(testCase.organizationSearchResult.Items[1].Users))
+			assert.Equal(t, testCase.expected[2], len(testCase.organizationSearchResult.Items[0].Tickets))
+			assert.Equal(t, testCase.expected[3], len(testCase.organizationSearchResult.Items[1].Users))
+		})
+	}
+}
--- lib/model/ticket_test.go
+++ lib/model/ticket_test.go
@@ -38,3 +38,30 @@ func TestTicketSearch(t *testing.T) {
 		})
 	}
 }
+
+func TestTicketDecorate(t *testing.T) {
+	testCases := []struct {
+		desc               string
+		ticketSearchResult TicketSearchResult
+		expected           []int
+	}{
+		{
+			desc:               "Ticket result is decorated succesfully, its number of users and tickets is 1 not 0",
+			ticketSearchResult: MockTicketSearchResult,
+			expected:           []int{1, 2, 1, 2, 1, 2},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.desc, func(t *testing.T) {
+			testCase.ticketSearchResult.Decorate(MockDataSet)
+			assert.Equal(t, testCase.expected[0], testCase.ticketSearchResult.Items[0].Submitter.ID)
+			assert.Equal(t, testCase.expected[1], testCase.ticketSearchResult.Items[0].Assignee.ID)
+			assert.Equal(t, testCase.expected[2], testCase.ticketSearchResult.Items[0].Organization.ID)
+			assert.Equal(t, testCase.expected[3], testCase.ticketSearchResult.Items[1].Submitter.ID)
+			assert.Equal(t, testCase.expected[4], testCase.ticketSearchResult.Items[1].Assignee.ID)
+			assert.Equal(t, testCase.expected[5], testCase.ticketSearchResult.Items[1].Organization.ID)
+		})
+	}
+}
